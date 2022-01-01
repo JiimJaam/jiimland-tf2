3220c32b55c2db9ce1d8d461adb0097da2f68dde
JiimJaam <jiimjaamjuump@gmail.com>
2022-01-01 16:33:02 -0500

    Add files via upload

---
+++ hlserver/tf2_2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+~/hlserver/tf2/srcds_run -steam_dir ~/hlserver/ -steamcmd_script ~/hlserver/tf2_ds.txt -autoupdate -console -game tf -timeout 0 +sv_shutdown_timeout_minutes 30 +sv_pure 0 +randommap +exec autoexec2.cfg +maxplayers 32 +tv_enable 1 -tvmasteronly
