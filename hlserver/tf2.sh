fc5e08e38a222db88756b5179817a20bad26700f
JiimJaam <jiimjaamjuump@gmail.com>
2022-01-01 14:06:24 -0500

    Added basic shell scripts

---
+++ hlserver/tf2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+~/hlserver/tf2/srcds_run -steam_dir ~/hlserver/ -steamcmd_script ~/hlserver/tf2_ds.txt -port 27015 -autoupdate -console -game tf -timeout 0 +sv_shutdown_timeout_minutes 30 +sv_pure 0 +randommap +maxplayers 32 +tv_enable 1 -tvmasteronly
